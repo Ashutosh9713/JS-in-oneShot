8c6ee6e7189fb70622fa4d5c410dfc86b92c51ff
Ashutosh sharma <112890513+Ashutosh9713@users.noreply.github.com>
2025-03-15 14:50:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code a newer version
+
+// alert(3+3); // we are using node.js 
+
+// console.log(3
+//     +
+//     3);//code redability should be high
+
+console.log(3+3);
+console.log("aashu") ;
+
+let Myname = "aashutosh";
+let age = 22;
+let landMark ="Kanker";
+
+
+// premitive data type
+
+// number => 2 to power of 52
+//bigint
+//string = ""
+//boolean = true/false;
+//null = standalone value
+//undefined= value is not asign
+//symbol = mostly in uniqueness
+
+console.log(typeof Myname);
+console.log(typeof null); // type iska object
+console.log(typeof undefined);
+console.log(typeof age);
+console.log(typeof landMark);
---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 132200;
+let accountEmail = "aashu@gmail.com";
+var accountPassword = "mypass";
+accountCity = "kanker";
+
+// accountId = 2222;
+accountEmail = "my@gmail.com";
+accountPassword = "yourpass";
+accountCity  = "raipur";
+
+// console.log(accountId);
+// console.table([accountEmail,accountPassword,accountCity]);
+
+/*
+prefer not to use var 
+because of issue in block scope and fuctional scope
+
+*/
